b22260df8a01ee51b85a9326a3a2f2fd2d2a2d65
Om <email.swar@gmail.com>
2019-10-01 11:30:27 +0530

    Pass arguments instead of values being hardcoded
    Arg 1: Type of files(EXT). Example: pdf or jpg
    Arg 2: Text to be replaced
    Arg 3: Text to replace with
     For instance $ ./replace_substring_in_files.sh JPG 'orginal substring' 'text to be replaced with'

--- replace_substring_in_files.sh
+++ replace_substring_in_files.sh
@@ -2,9 +2,14 @@
 # in current directory
 #!/bin/bash
 IFS=$'\n'
-FILES=$(ls *pdf)
+FILES=$(ls *${1})
+echo "Arg 1: Type of files(EXT). Example: pdf or jpg"
+echo "Arg 2: Text to be replaced"
+echo "Arg 3: Text to replace with"
+echo " For instance $ ./replace_substring_in_files.sh JPG 'orginal substring' 'text to be replaced with' "
+echo " For files with ${1} Ext, you want to change the substring ${2} to ${3} "
 for FILE in $FILES
 do
- echo "renaming ${FILE} to ${FILE// ( old text )/new text} "
- mv ${FILE} ${FILE// ( old text )/new text}  
+ echo "renaming ${FILE} to ${FILE//$2/$3} "
+ mv ${FILE} ${FILE//$2/$3}  
 done
